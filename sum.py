fa73ceb61715e48e928848eeff1147e306b97ae3
JesusJaimesN <ingjnajera@gmail.com>
2023-02-06 19:39:24 -0600

    agregamos sum.py y prueba .py

---
+++ sum.py
@@ -0,0 +1,14 @@
+# ********************
+# LA SUMA M�S SENCILLA
+# ********************
+
+
+def run(a: int, b: int) -> int:
+    # TU C�DIGO AQU�
+    result = a+b
+
+    return result
+
+
+if __name__ == '__main__':
+    run(3, 4)
